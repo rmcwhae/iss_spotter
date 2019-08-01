d3aece5398ce8aa519b4cd739218f2aee13834c1
Russell McWhae <russell.mcwhae@gmail.com>
2019-08-01 22:08:19 +0000

    Adds error handling

--- index2.js
+++ index2.js
@@ -3,4 +3,7 @@ const { nextISSTimesForMyLocation, printPassTimes } = require('./iss_promised');
 nextISSTimesForMyLocation()
   .then((passTimes) => {
     printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
   });
