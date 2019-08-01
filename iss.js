53f5d95bd00c7478eca6790ea51635a79dc39602
Russell McWhae <russell.mcwhae@gmail.com>
2019-08-01 16:17:31 +0000

    fetchMyIP updated to address error handling

--- iss.js
+++ iss.js
@@ -10,13 +10,22 @@ const request = require('request');
 
 const fetchMyIP = function(callback) {
   request('https://api.ipify.org/?format=json', (error, response, body) => {
+    // error can be set if invalid domain, user is offline, etc.
     if (error) {
       callback(error, null);
-    } else {
-      const data = JSON.parse(body);
-      let IP = data["ip"];
-      callback(null, IP);
+      return;
     }
+    //if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    // if we get here, all's well and we got the data
+    const data = JSON.parse(body);
+    let IP = data["ip"];
+    callback(null, IP);
       
   });
 };
